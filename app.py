14c8c80508d90fa2fefd39424d3dbd08bafb475f
Radu Ionut-Alexandru <radui1398@gmail.com>
2020-05-21 23:02:15 +0300

    Deploy improvements

--- app.py
+++ app.py
@@ -49,4 +49,7 @@ def today():
         print(e)
         return Response("Invalid request.", status=400, mimetype='application/json')
 
-app.run()
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
